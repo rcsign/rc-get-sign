7baab550ade1205c7d6ccc3e5ad6b1670be9a3f8
Loch9 <lochie.rickerby@gmail.com>
2024-05-31 12:33:00 +0100

    Rearrange, add azure api

---
+++ rcsign/Controllers/InstagramProxyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rcsign.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InstagramProxyController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public InstagramProxyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet("access-token")]
+        public async Task<IActionResult> GetAccessToken(string clientId, string clientSecret, string redirectUri, string code)
+        {
+            var response = await _httpClient.PostAsync(
+                "https://api.instagram.com/oauth/access_token",
+                new FormUrlEncodedContent([
+                    new("client_id", clientId),
+                    new("client_secret", clientSecret),
+                    new("grant_type", "authorization_code"),
+                    new("redirect_uri", redirectUri),
+                    new("code", code)
+                ])
+            );
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            return Ok(content);
+        }
+    }
+}
