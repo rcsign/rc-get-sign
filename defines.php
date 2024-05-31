2fd75b2d454d39186cec3306278f23f2d17b759b
Loch9 <lochie.rickerby@gmail.com>
2024-05-31 16:08:14 +0100

    php

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '440417048755047' );
+	define( 'INSTAGRAM_APP_SECRET', '6f3721317ededdbae6ff0437ae0e4892' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://rcsign.github.io' );
